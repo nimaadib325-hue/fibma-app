3d353b62ff9dab606d84e87448c69f0e2b1298d9
nimaadib325-hue <nimaadib325@gmail.com>
2026-06-07 11:20:17 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'fibma-v1';
+const ASSETS = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.url.includes('tradingview') || e.request.url.includes('s3.')) {
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
